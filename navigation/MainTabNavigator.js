fc3536b02264d9bc70047dbb2a86c8afa1c2bc20
Alejandro Martinez <hello@alejandrom.xyz>
2018-10-17 10:19:25 -0700

    Redo

---
+++ navigation/MainTabNavigator.js
@@ -0,0 +1,91 @@
+import React from 'react';
+import { Image, StyleSheet, Platform } from 'react-native';
+import { createStackNavigator, createBottomTabNavigator } from 'react-navigation';
+
+import TabBarIcon from '../components/TabBarIcon';
+import HomeScreen from '../screens/HomeScreen';
+import LinksScreen from '../screens/LinksScreen';
+import SettingsScreen from '../screens/SettingsScreen';
+const iconCalendar = require('../assets/images/home.png');
+const iconMap = require('../assets/images/map.png');
+
+const HomeStack = createStackNavigator({
+  Home: HomeScreen,
+});
+
+const styles = StyleSheet.create({
+  tabBarItemContainer: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderBottomWidth: 2,
+    borderBottomColor: "#fff",
+    paddingHorizontal: 10,
+  },
+  tabBarIcon: {
+    width: 23,
+    height: 23,
+  },
+  tabBarIconFocused: {
+    tintColor: "#000",
+  },
+  headerContainer: {
+    height: 70,
+    alignItems: 'center',
+    justifyContent: 'flex-end',
+    paddingBottom: 10,
+  },
+  headerImage: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    height: 70,
+  },
+  headerCaption: {
+    fontFamily: "Times",
+    color: "#000",
+    fontSize: 18,
+  },
+});
+
+HomeStack.navigationOptions = {
+  tabBarLabel: 'Home',
+  tabBarIcon: ({ focused }) => (
+    <Image resizeMode="contain" source={iconCalendar}
+    style={[ styles.tabBarIcon, focused && styles.tabBarIconFocused ]} />
+  ),
+};
+
+const LinksStack = createStackNavigator({
+  Links: LinksScreen,
+});
+
+LinksStack.navigationOptions = {
+  tabBarLabel: 'Map',
+  tabBarIcon: ({ focused }) => (
+    <Image resizeMode="contain" source={iconMap}
+    style={[ styles.tabBarIcon, focused && styles.tabBarIconFocused ]} />
+  ),
+};
+
+const SettingsStack = createStackNavigator({
+  Settings: SettingsScreen,
+});
+
+SettingsStack.navigationOptions = {
+  tabBarLabel: 'Settings',
+  tabBarIcon: ({ focused }) => (
+    <TabBarIcon
+      focused={focused}
+      name={Platform.OS === 'ios' ? `ios-options${focused ? '' : '-outline'}` : 'md-options'}
+    />
+  ),
+};
+
+export default createBottomTabNavigator({
+  HomeStack,
+  LinksStack,
+  SettingsStack,
+});
---
+++ screens/HomeScreen.js
@@ -0,0 +1,39 @@
+import Expo from 'expo';
+import React, { Component } from 'react';
+import { Platform, Text, View, StyleSheet } from 'react-native';
+import { Constants, Location, Permissions } from 'expo';
+
+const GEOLOCATION_OPTIONS = { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 };
+
+export default class HomeScreen extends Component {
+  state = {
+    location: { coords: {latitude: 0, longitude: 0}},
+  };
+
+  componentWillMount() {
+    Location.watchPositionAsync(GEOLOCATION_OPTIONS, this.locationChanged);
+  }
+
+  locationChanged = (location) => {
+    region = {
+      latitude: location.coords.latitude,
+      longitude: location.coords.longitude,
+      latitudeDelta: 0.001,
+      longitudeDelta: 0.05,
+    },
+    this.setState({location, region})
+  }
+
+  render() {
+    return (<View>
+        <Expo.MapView
+          style={{ height: "100%" }}
+          showsUserLocation={true}
+          region={this.state.region}
+        />
+        <Text>Test</Text>
+    </View>);
+  }
+}
+
+Expo.registerRootComponent(HomeScreen);
