87ced97e681055a427533c168dabc09383108742
AH-C <ian.postel.476@my.csun.edu>
2018-10-17 22:21:55 -0700

    leadup to a login

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
+import LoginTabNavigator from './LoginTabNavigator';
 
 export default createSwitchNavigator({
-  // You could add another route here for authentication.
-  // Read more at https://reactnavigation.org/docs/en/auth-flow.html
+  Login: LoginTabNavigator,
   Main: MainTabNavigator,
-});
+},
+{
+  initialRouteName: 'Login',
+}
+);
--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { AsyncStorage } from 'react-native';
 import { ExpoConfigView } from '@expo/samples';
 
 export default class SettingsScreen extends React.Component {
@@ -9,6 +10,15 @@ export default class SettingsScreen extends React.Component {
   render() {
     /* Go ahead and delete ExpoConfigView and replace it with your
      * content, we just wanted to give you a quick view of your config */
+    
+    // TODO: Add a button that calls _signOutAsync
+    
     return <ExpoConfigView />;
   }
+
+
+  _signOutAsync = async () => {
+    await AsyncStorage.clear();
+    this.props.navigation.navigate('Login');
+  };
 }
