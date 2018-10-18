29513feb09b0c59d9fe11307c7df215509ca077d
AH-C <ian.postel.476@my.csun.edu>
2018-10-17 22:48:53 -0700

    "Wait am I immortal?" "I don't know, let's see."

--- navigation/AppNavigator.js
+++ navigation/AppNavigator.js
@@ -3,10 +3,12 @@ import { createSwitchNavigator } from 'react-navigation';
 
 import MainTabNavigator from './MainTabNavigator';
 import LoginTabNavigator from './LoginTabNavigator';
+import LoginScreen from '../screens/LoginScreen';
 
 export default createSwitchNavigator({
   Login: LoginTabNavigator,
   Main: MainTabNavigator,
+  LoginScr: LoginScreen,
 },
 {
   initialRouteName: 'Login',
